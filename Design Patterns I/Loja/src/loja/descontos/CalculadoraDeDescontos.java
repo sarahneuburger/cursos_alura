c6b5b9438ce83d65486b5a2cd9b422525c28bf60
sarahneuburger <61321914+sarahneuburger@users.noreply.github.com>
2021-07-22 21:58:49 -0300

    Atualização Aula 2

--- Design Patterns I/Loja/src/loja/descontos/CalculadoraDeDescontos.java
+++ Design Patterns I/Loja/src/loja/descontos/CalculadoraDeDescontos.java
@@ -6,9 +6,17 @@ import loja.orcamento.Orcamento;
 
 /**
  * 
- *Chain of Resposability
+ * Chain of Resposability
  *
- *
+ * Não sabemos de cara qual desconto deverá ser aplicado. Neste caso, precisa
+ * ser verificado cada caso. Uma classe que possui a assinatura do desconto e a
+ * variável para setar o próximo desconto que será verificado, via construtor.
+ * 
+ * O padrão conhecido como Chain Of Responsibility, corrente de responsabilidades 
+ * onde eu tenho uma corrente, uma sequência, um fluxo de responsabilidades e eu 
+ * vou verificando qual delas precisa ser aplicada. Se uma não for aplicada, ele 
+ * está ligado e tem um elo com a próxima e ele vai chamar a próxima. Se for aplicada, 
+ * aplicou e encerrou o assunto. 
  *
  */
 
@@ -16,8 +24,7 @@ public class CalculadoraDeDescontos {
 
 	public BigDecimal calcular(Orcamento orcamento) {
 		Descontos desconto = new DescontoParaOrcamentoMaisCincoItens(
-				new DescontoParaOrcamentoMaiorQueQuinhentos(
-						new SemDesconto()));
+				new DescontoParaOrcamentoMaiorQueQuinhentos(new SemDesconto()));
 		return desconto.calcular(orcamento);
 	}
 
