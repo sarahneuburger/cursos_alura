df6a3fa2b7cb437f3988d18906f165c045e2f008
sarahneuburger <61321914+sarahneuburger@users.noreply.github.com>
2021-08-08 19:06:28 -0300

    Aula 1 - Arquitetura de Software

---
+++ Arquitetura de Software/escola/src/main/java/escola/Aluno.java
@@ -0,0 +1,18 @@
+package escola;
+
+/**
+ * ID faz parte da infraestrutura (banco) e não ao domínio.
+ * 
+ * Nesse contexto, entidade é a classe que possui o identificador único
+ * de um objeto.
+ */
+public class Aluno {
+	
+	// Serviço como identificado único
+	private String cpf;
+	private String nome;
+	private String email;
+	
+	
+	
+}
