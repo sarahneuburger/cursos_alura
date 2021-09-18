860cb7e1e7a574344f18bd6332a0f5c7cb0148bd
sarahneuburger <61321914+sarahneuburger@users.noreply.github.com>
2021-09-18 20:08:50 -0300

    Aula 6 - Autoboxing - Java e java.util

--- Java e java.util/bytebank-herdado-conta/src/br/com/bytebank/banco/test/util/Teste.java
+++ Java e java.util/bytebank-herdado-conta/src/br/com/bytebank/banco/test/util/Teste.java
@@ -1,5 +1,19 @@
 package br.com.bytebank.banco.test.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Teste {
+	
+	public static void main(String[] args) {
+		
+		int idade = 26;
+		List numeros = new ArrayList();
+		
+		// Autoboxing, apesar do List receber apenas objetos e idade ser primitiva, o 
+		// Java converte automaticamente para o tipo object correspodente, neste caso
+		// o Integer.
+		numeros.add(idade);
+	}
 
 }
