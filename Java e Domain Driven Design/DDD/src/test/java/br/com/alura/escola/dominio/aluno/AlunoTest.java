7a1bb4afbdae7ce7594c0cdae09fe000b6d32793
sarahneuburger <61321914+sarahneuburger@users.noreply.github.com>
2021-09-27 21:03:01 -0300

    Aula 1 e início da 2 - DDD

---
+++ Java e Domain Driven Design/DDD/src/test/java/br/com/alura/escola/dominio/aluno/AlunoTest.java
@@ -0,0 +1,40 @@
+package br.com.alura.escola.dominio.aluno;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class AlunoTest {
+
+	private Aluno aluno;
+
+	@BeforeEach
+	public void beforeEach() {
+		this.aluno = new Aluno(new CPF("123.456.789-10"), "Fulano da Silva", new Email("fulano@email.com"));
+	}
+
+	@Test
+	void deveriaPermitirAdicionar1Telefone() {
+		this.aluno.adicionarTelefone("11", "999887755");
+		assertEquals(1, this.aluno.getTelefones().size());
+	}
+
+	@Test
+	void deveriaPermitirAdicionar2Telefone() {
+		this.aluno.adicionarTelefone("11", "999999999");
+		this.aluno.adicionarTelefone("11", "888888888");
+		assertEquals(2, this.aluno.getTelefones().size());
+	}
+
+	@Test
+	void deveriaPermitirAdicionar3Telefone() {
+		assertThrows(IllegalArgumentException.class, () -> {
+			this.aluno.adicionarTelefone("11", "777777777");
+			this.aluno.adicionarTelefone("11", "999999999");
+			this.aluno.adicionarTelefone("11", "888888888");
+		});
+	}
+
+}
