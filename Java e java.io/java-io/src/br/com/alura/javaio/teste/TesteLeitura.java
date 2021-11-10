5e77c701bdeafea0b593b2bc815f54a2b72ff1e1
sarahneuburger <61321914+sarahneuburger@users.noreply.github.com>
2021-11-09 22:01:21 -0300

    Aula 1 - Java e java.io

---
+++ Java e java.io/java-io/src/br/com/alura/javaio/teste/TesteLeitura.java
@@ -0,0 +1,31 @@
+package br.com.alura.javaio.teste;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class TesteLeitura {
+
+	public static void main(String[] args) throws IOException {
+		
+		// Fluxo de entrada com arquivo
+		FileInputStream fis = new FileInputStream("lorem.txt");
+		InputStreamReader isr = new InputStreamReader(fis);
+		// Classe para ler os caracteres
+		BufferedReader br = new BufferedReader(isr);
+		
+		// Ler uma linha
+		String linha = br.readLine();
+		
+		while(linha != null) {
+			System.out.println(linha);
+			linha = br.readLine();
+		}
+		
+		// Assim como abrir, é preciso fechar a chamada
+		br.close();
+
+	}
+
+}
