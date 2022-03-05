09d5f46d205c569f2521b0139142824b9f511601
sarahneuburger <61321914+sarahneuburger@users.noreply.github.com>
2022-03-05 18:43:46 -0300

    Estrutura de Dados - Aula 1

---
+++ Estrutura de Dados/ed/src/ed/Aluno.java
@@ -0,0 +1,26 @@
+package ed;
+
+public class Aluno {
+
+	private String name;
+
+	public Aluno(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		Aluno outro = (Aluno) obj;
+		return outro.getName().equals(this.name);
+	}
+
+	@Override
+	public String toString() {
+		return name;
+	}
+
+}
---
+++ Estrutura de Dados/ed/src/ed/Vetor.java
@@ -0,0 +1,83 @@
+package ed;
+
+import java.util.Arrays;
+
+public class Vetor {
+
+	private Aluno[] alunos = new Aluno[100];
+	private int totalDeAlunos = 0;
+
+	// Inserir um aluno depende do tamanho da lista. Fica mais demorado conforme a
+	// lista tem
+	// mais objetos. Adicionado contador para já ter a posição livre armazenada
+	public void adicionar(Aluno aluno) {
+//		for (int i = 0; i < alunos.length; i++) {
+//			if(alunos[i] == null) {
+//				alunos[i] = aluno;
+//				break;
+//			}
+//			
+//		}
+		// Leva tempo constante, idependente da quantidade de dados no Array
+		// Performance será sempre a mesma
+		this.alunos[totalDeAlunos] = aluno;
+		totalDeAlunos++;
+	}
+
+	private boolean posicaoValida(int posicao) {
+		return posicao >= 0 && posicao <= totalDeAlunos;
+	}
+
+	// Para inserir em posição específica, é preciso pegar todos que estavam a
+	// partir da mesma e
+	// empurrar para a direita
+	// Também O(N), pois o tamanho da lista influencia
+	public void adicionar(Aluno aluno, int posicao) {
+		if (!posicaoValida(posicao)) {
+			throw new IllegalArgumentException("Posição inválida");
+		}
+
+		for (int i = totalDeAlunos - 1; i == posicao; i -= 1) {
+			alunos[i + 1] = alunos[i];
+		}
+		alunos[posicao] = aluno;
+		totalDeAlunos++;
+	}
+
+	private boolean posicaoOcupada(int posicao) {
+		return posicao >= 0 && posicao < totalDeAlunos;
+	}
+
+	public Aluno pega(int posicao) {
+		if (!posicaoOcupada(posicao)) {
+			throw new IllegalArgumentException("Posicação ocupada.");
+		}
+		return alunos[posicao];
+	}
+
+	// Não há opção além de percorrer o array
+	// Só vai varrer o array até a posição que sabemos que tem alunos
+	// Tempo de execução varia pela quantidade de elementos O(N)
+	public boolean contem(Aluno aluno) {
+		for (int i = 0; i < totalDeAlunos; i++) {
+			if (aluno.equals(alunos[i]))
+				return true;
+		}
+		return false;
+	}
+
+	public int tamanho() {
+		return totalDeAlunos;
+	}
+
+	public String toString() {
+		return Arrays.toString(alunos);
+	}
+
+	public void remove(int posicao) {
+		for (int i = posicao; i < totalDeAlunos; i++) {
+			alunos[i] = alunos[i + 1];
+		}
+	}
+
+}
